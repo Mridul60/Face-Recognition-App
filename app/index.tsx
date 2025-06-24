17a7a517ad02400624652d318cd4b033e38c1bd9
Faruk Khan <faruk.khan.cse@gmail.com>
2025-06-24 13:33:05 +0530

    add commands.txt

--- app/index.tsx
+++ app/index.tsx
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
     },
     section: {
         marginBottom: 20,
-        // backgroundColor: 'red',
+        // backgroundColor: 'red',,
     },
     image: {
         width: 200,
