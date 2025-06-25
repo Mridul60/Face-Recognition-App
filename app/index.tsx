60a52d913d33b438835675b4cabe936e6c01f321
Mridul60 <mridulroy543@gmail.com>
2025-06-25 15:39:10 +0530

    dashboard  basic

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,49 @@
+import { Redirect, useRouter } from 'expo-router';
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
-import styles from './styles'
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import styles from './styles';
 
 export default function Index() {
+    let is_dev_mode = false;
     const router = useRouter();
-    return (
-        <View style={styles.container}>
-            <View style={styles.topSection}>
-                <Image
-                    source={require('../assets/images/face.png')}
-                    style={styles.image}
-                />
-                <Text style={styles.appName}>FRAT</Text>
-                <Text style={styles.appAbout}>
-                    Face Recognition Attendance Tracking
-                </Text>
-            </View>
 
-            <View>
-                <View style={styles.buttonRow}>
-                    <TouchableOpacity style={styles.button} onPress={() => router.push('/views/login')}>
-                        <Text style={styles.buttonText}>Login</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={styles.buttonText}>Register</Text>
+    if (is_dev_mode) {
+        return <Redirect href='/views/dashboard' />;
+    }
+    else{
+        return (
+            <View style={styles.container}>
+                <View style={styles.topSection}>
+                    <Image
+                        source={require('../assets/images/face.png')}
+                        style={styles.image}
+                    />
+                    <Text style={styles.appName}>FRAT</Text>
+                    <Text style={styles.appAbout}>
+                        Face Recognition Attendance Tracking
+                    </Text>
+                </View>
+    
+                <View>
+                    <View style={styles.buttonRow}>
+                        <TouchableOpacity style={styles.button} onPress={() => router.push('/views/login')}>
+                            <Text style={styles.buttonText}>Login</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}>Register</Text>
+                        </TouchableOpacity>
+                    </View>
+    
+                    <View style={styles.separator} />
+    
+                    <TouchableOpacity style={styles.googleButton}>
+                        <Text style={styles.buttonText}>Continue with Google</Text>
                     </TouchableOpacity>
                 </View>
-
-                <View style={styles.separator} />
-
-                <TouchableOpacity style={styles.googleButton}>
-                    <Text style={styles.buttonText}>Continue with Google</Text>
-                </TouchableOpacity>
             </View>
-        </View>
-    );
-}
-
-const Separator = () => <View style={styles.separator}/>;
-
+        );
+    }
+        
+    
+            
+        }        
