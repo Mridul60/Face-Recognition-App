336cb0b787841d7613de57613501d7a3b68424ba
Faruk Khan <faruk.khan.cse@gmail.com>
2025-07-14 13:32:30 +0530

    UI Restoration

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Redirect } from "expo-router";
 import Toast from 'react-native-toast-message';
 
 export default function Index() {
-  const isBeingDesigned = true;
+  const isBeingDesigned = false;
   const isLoggedIn = false;
 
   if (isBeingDesigned) {
